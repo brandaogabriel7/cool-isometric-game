62e5f35107855cac33c0cdc62669bdb951c2e663
Gabriel Silva Brandão <gabriel.silva.brandao7@gmail.com>
2019-06-25 18:42:14 -0300

    Create script to ignore collisions between player and path

---
+++ Cool_Isometric_Game/Assets/Scripts/PathTilemapCollisionIgnore.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PathTilemapCollisionIgnore : MonoBehaviour
+{
+    void Start()
+    {
+        Tilemap[] tilemaps = FindObjectsOfType<Tilemap>();
+		GameObject pathGO = new GameObject();
+		for(int i = 0; i < tilemaps.Length; i++) {
+			if(tilemaps[i].gameObject.layer == 8) 
+				pathGO = tilemaps[i].gameObject;
+		}
+		Physics2D.IgnoreCollision(pathGO.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+    }
+}
